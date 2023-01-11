c53c8a825d5715f2189fb395d18b4b5ff8b3dbde
sreeja-chithirala <103505618+sreeja-chithirala@users.noreply.github.com>
2023-01-12 00:16:58 +0530

    Create no_of_words_infile.py

---
+++ no_of_words_infile.py
@@ -0,0 +1,17 @@
+f=open("python_2file.txt","r")
+data=f.read()     #reads the content of the file and stores that in the form of string data
+print("The data in the file is: ",data)
+c=0
+for i in range(0,len(data)):
+    if data[i]!=' ' and  data[i]!='.' : 
+        i=i+1
+    else :
+        i=i+1
+        c=c+1     #if we complete moving after a word the c variable gets incremented
+    
+print("The no of words in the above txt file are ",c)
+
+'''
+The data in the file is:  Hi!.My name is Sreeja Chithirala.My roll no is AP20110010321.Iam from CSE section 'E'.
+The no of words in the above txt file are  16
+'''
